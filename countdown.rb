72165d10c83186acca8843278e39c974b10729d0
Olivia <ortufte@gmail.com>
2019-09-13 16:17:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@ def countdown(number)
   number = -10
   while number < 0
     puts " -#{number} SECOND(S)!"
-    number -= 1 
+    number += 1 
   end
   if number == 0 
     return "HAPPY NEW YEAR!"
